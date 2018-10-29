ae25f895953808b4d072a6f4df2b0d6bbc693814
Yossrizal Ramadhan <yossrizalr@gmail.com>
2018-10-29 13:06:25 +0700

    Add files via upload

---
+++ java/com/example/yoss/myrecyclerview/President.java
@@ -0,0 +1,44 @@
+package com.example.yoss.myrecyclerview;
+
+public class President {
+    private String name, remarks, photo, year, partai;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+    public String getPartai() {
+        return partai;
+    }
+
+    public void setPartai(String partai) {
+        this.partai = partai;
+    }
+}
